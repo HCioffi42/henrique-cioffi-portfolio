d114d4bf3f5e7046f80b855c6f2ef5729d4ca398
Henrique Cioffi <henriquecioffipr@hotmail.com>
2026-04-20 14:12:52 -0300

    fix: fixed some issues where the Dashboard Page loaded from Legacy collumns in the database

--- MeuSitePessoal.Application/Articles/Queries/GetAllArticles/GetAllArticlesQuery.cs
+++ MeuSitePessoal.Application/Articles/Queries/GetAllArticles/GetAllArticlesQuery.cs
@@ -1,9 +1,10 @@
 using MediatR;
 using MeuSitePessoal.Application.Common.Models;
-using MeuSitePessoal.Domain;
-using MeuSitePessoal.Domain.Entities;
 
 namespace MeuSitePessoal.Application.Articles.Queries.GetAllArticles;
 
-// The query now accepts pagination parameters and returns a PagedList instead of a simple IEnumerable.
-public record GetAllArticlesQuery(int PageNumber = 1, int PageSize = 10) : IRequest<PagedList<Article>>;
+/**
+ * Represents a query to retrieve a paginated list of all articles.
+ * Returns ArticleResponse objects to support localized content display in Administrative UI.
+ */
+public record GetAllArticlesQuery(int PageNumber = 1, int PageSize = 10) : IRequest<PagedList<ArticleResponse>>;
